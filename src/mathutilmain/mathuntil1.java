793b43cf7938270ecd3be4be28969f02a2f05744
minhphuong-code <tuminhphuong.nk@gmail.com>
2020-11-14 14:08:43 +0700

    1st upload

---
+++ src/mathutilmain/mathuntil1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mathutilmain;
+
+import phuong.util.Mathutility;
+
+/**
+ *
+ * @author AlvinTu
+ */
+public class mathuntil1 {
+
+    public static void main(String[] args) {
+        long expected = 120;
+        long actual = Mathutility.getFactorial(5);
+        //in ra kieemr tra su khop nhau gia cai ta muon va cais ta lam
+        System.out.println("5! expected: " + expected);
+        System.out.println("5! actual: " + actual);
+
+        //tiếp tục cho các tình huống 6! 7! ......
+        //             các tình huống cần phải test ta gọi là test cases
+        // về lý thuyết ta phải test trước hết các tình huống.....
+        System.out.println("6!: 720, actual:" + Mathutility.getFactorial(6));
+        System.out.println("0!: 1, actual:" + Mathutility.getFactorial(1));
+
+        System.out.println("expected:  IllegalArgumentException: " + Mathutility.getFactorial(-5));      //tao kì vọng mày ném về ngoại lệ, phải là ngoại lệ
+
+    }
+}
---
+++ src/phuong/util/Mathutility.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package phuong.util;
+
+/**
+ *
+ * @author AlvinTu
+ */
+public class Mathutility {
+    //làm bộ thư viện fake giống y chang math
+    //Math.pi, .abs(), .pow(a,b) a^b
+    //phàm là thu viện thi phải xài static khi không cần lưu info cho riêng mình
+    //còn thư viện mà vẫn cần lưu info riêng thì vẫn non-static như thường  
+
+    public static final double PI = 3.1215;
+
+    // tiện ích tính giai thừ n!=1,2,3,4,..........n
+    //n phải là từ 0 trở lên
+    //0! = 1!=1
+    //âm gia thừa vô nghĩa
+    //giai thừ tnawg cục nhanh,21! long ko chứa được
+    // bài này mình sẽ chặn giai thừ từ 0..21
+    //âm hoặc 21 chửi, không thèm tính
+    public static long getFactorial(int n) {
+        if (n < 0 || n > 20) {
+            throw new IllegalArgumentException("n must be between 0...20");
+        }
+        if (n == 0 || n == 1) {
+            return 1; // đặt biệt , return luôn
+        }        // đứa nào viết else ở đây, trừ điểm 
+        // mặc nhiên ở đây hiểu rằng n= 2..20
+        long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
