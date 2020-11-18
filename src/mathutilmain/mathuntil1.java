3c2e4265a0441b1d23609dcbad5e0f753bada4a0
minhphuong-code <tuminhphuong.nk@gmail.com>
2020-11-18 09:04:31 +0700

    add JUnit code $ .jar(s)

--- src/mathutilmain/mathuntil1.java
+++ src/mathutilmain/mathuntil1.java
@@ -32,3 +32,27 @@ public class mathuntil1 {
 
     }
 }
+// mình muốn kết luận hàm ngon, mình phải test hết các trường hợp xài hàm
+// trường hợp 5! 0! -5! .....
+// mình phải thiết kế các test case, tình huống test để sau đó mình verify 
+// giữa các giá trị trả về có khóp với expected cái mình dự đoán hàm chạy trả về
+// và nếu mọi thư skhopws tức là actual==expected mình mới mạnh tay dám gáy hàm ngon
+// cách nafuy có nhược điểm phải nhìn bằng mắt so sánh kết quả từng case 
+
+// cách này ổn, nhưng tiềm ẩn nhiều sai sót do nhìn bằng mắt nhiều case
+// cách nâng cao 
+// cũng nhìn bằng mắt nhưng nhìn chỉ 2 màu xanh và đỏ cho mọi test case
+// nếu bạn chạy hàm 100 tham số , chạy hamnf 100 tình huống khác nhau
+// bản chỉ cần nhìn duy nhất 2 màu xanh đỏ
+// xanh code ổn cho mọi test case mọi tình huống gọi hàm 
+// mọi cas ohari có expected= actual thì mới xanh
+// đỏ, mọi case màu xanhg chỉ ít nhất 1 thằng đỏ kết liaanj cả đám đỏ 
+// hàm đúng cho 99 trường hợp,expected== actual cho mọi 99% tình huống
+// chỉ có 1 tình huống sai , không khớp kết luận luôn hàm ko ổn đinhhj
+// ai dám xài khi không ổn định , ổn định cà chỉnh xác cho mọi trường hợp
+
+// MUỐN LÀM ĐƯỢC ĐIỀU NAY, CẦN SỰ PHỤ GIÚP CỦA CAC THƯ VIỆN BÊN NGOÀI JDK GỌI LÀ 
+// UNIT TEST FRAMEWORK
+// CÁC BỘ UNIT TEST NỔI TIẾNG
+// bữa nay mình học xanh đỏ qua JUnit
+// vẫn với mục tiêu test hàm có ngon không, nhưng chỉ nhìn đúng 2 màu, không care chi tiết các tình huống chạy hàm
---
+++ test/phuong/util/test/MathUtilityTest.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package phuong.util.test;
+
+import static phuong.util.Mathutility.*;
+// chỉ có từ JDK5?, hàm static gọi mà hong cần tên class chấm 
+// do mình đã chơi trò khai báo sẵn tên claas và tên hàm static qua lệnh
+// import static
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+/**
+ *
+ * @author AlvinTu
+ */
+public class MathUtilityTest {
+    // viết ra những tình huống xài hàm getFactorial()
+    // kiểm tra xem actual==expected hay không?? như truyền thống bên main()
+    // nhưng ở đây xịn sò hơn, show ra Xanh Đỏ
+    // ngoài ra nó còn dùng cho việc CI = Continuous Delivery
+    //                               CT = Continuous Test
+    //                               DevOps= Development & Operation
+    // ngoài doanh nghiệp đang xài 
+    // app liên tục đc kiểm tra chất lượng code, build ra file .jar .war
+    // đảy lên 1 server trung gian - Staging Environment để cho nhóm test
+    // sẵn sàng test kĩ
+    // sau đó có thể đẩy thẳng lên PRODUCTION SERVER, máy chạy thật ở nơi khách hàng
+    // mọi việc sẽ bắt đầu kể từ lúc Developer push code lên GitHub
+    
+    @Test // từ khóa này tương đương biến hàm này thành public static void main()\
+    // trong hàm này mình sẽ show ra màu xanh dỏ dựa trên sự khóp nhau của expected và actual qua hàm asserEquals()... mà thư viện cung cấp
+    // asser...() đủ các loại assert trên đời, assert(v) so sánh
+    // thay vò sao sánh banwfh mắt, ta nhờ hàm so sánh giùm, thảy về màu giùm ta
+    public void getFactorial_RunsWell_IfValidArgument(){
+        assertEquals(120,getFactorial(5));
+        assertEquals(24,getFactorial(4));
+        assertEquals(720,getFactorial(6));
+        assertEquals(1,getFactorial(0));
+                // hãy tính dùm tui 5! coi nó có là 120 hong?
+        // có thì báo xanh, không thì báo đỏ 
+    }
+}
